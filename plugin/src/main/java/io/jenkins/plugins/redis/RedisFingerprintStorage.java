4a746c0248481a20037e5e8148d51af61d9dc648
Sumit Sarin <sumitsarinofficial@gmail.com>
2020-05-22 19:26:42 +0530

    Implement save and load functions for POC

--- plugin/src/main/java/io/jenkins/plugins/redis/RedisFingerprintStorage.java
+++ plugin/src/main/java/io/jenkins/plugins/redis/RedisFingerprintStorage.java
@@ -1,26 +1,75 @@
 package io.jenkins.plugins.redis;
 
-import hudson.model.Fingerprint;
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.BulkChange;
+import hudson.Extension;
+import hudson.Util;
 import hudson.model.Job;
+import hudson.model.Fingerprint;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import hudson.util.PersistedList;
+import jenkins.model.FingerprintFacet;
+import org.jenkinsci.main.modules.instance_identity.InstanceIdentity;
+import redis.clients.jedis.Jedis;
 
 /**
  * Pluggable external fingerprint storage for fingerprints in Redis.
  */
 
-public class RedisFingerprintStorage {
+@Extension
+public class RedisFingerprintStorage{
+
+    private String instanceId;
+    private static final Logger logger = Logger.getLogger(Fingerprint.class.getName());
+
+    RedisFingerprintStorage() throws IOException{
+        setInstanceId();
+    }
+
+    private void setInstanceId() throws IOException{
+        try {
+            instanceId = Util.getDigestOf(new ByteArrayInputStream(InstanceIdentity.get().getPublic().getEncoded()));
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "Failed to obtain Instance ID. "+e);
+            throw e;
+        }
+    }
 
     /**
      * Saves the given fingerprint.
      */
-    public boolean save(Fingerprint fp){
-        return true;
+    public synchronized void save(Fingerprint fp) throws IOException {
+        if(BulkChange.contains(fp))
+            return;
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server successfully");
+        StringWriter writer = new StringWriter();
+        fp.getXStream().toXML(fp, writer);
+        jedis.set(instanceId+fp.getHashString(), writer.toString());
     }
 
     /**
      * Returns the fingerprint associated with the given md5sum and the Jenkins instance ID, from the storage.
      */
-    public Fingerprint load(byte[] md5sum){
-        return null;
+    public @CheckForNull Fingerprint load(@NonNull String md5sum) throws IOException {
+        Jedis jedis = new Jedis("localhost");
+        Object loaded = jedis.get(instanceId+md5sum);
+        if (!(loaded instanceof Fingerprint)) {
+            throw new IOException("Unexpected Fingerprint type. Expected " + Fingerprint.class + " or subclass but got "
+                    + (loaded != null ? loaded.getClass() : "null"));
+        }
+        Fingerprint f = (Fingerprint) loaded;
+//        if (f.facets==null)
+//            f.facets = new PersistedList<>(f);
+//        for (FingerprintFacet facet : f.facets)
+//            facet._setOwner(f);
+        return f;
     }
 
     /**
@@ -68,5 +117,6 @@ public class RedisFingerprintStorage {
     public Fingerprint[] trace(Job j, int build){
         return null;
     }
+
 
 }
