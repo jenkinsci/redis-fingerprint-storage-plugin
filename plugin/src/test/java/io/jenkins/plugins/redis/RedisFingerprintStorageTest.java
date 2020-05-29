3cae49a3fd7494f9d8c5956318ae7b93157500f4
Sumit Sarin <sumitsarinofficial@gmail.com>
2020-05-30 02:43:20 +0530

    Rectify package name

--- plugin/src/test/java/io/jenkins/plugins/redis/RedisFingerprintStorageTest.java
+++ plugin/src/test/java/io/jenkins/plugins/redis/RedisFingerprintStorageTest.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package io.jenkins.plugins;
+package io.jenkins.plugins.redis;
 
 import hudson.Util;
 import hudson.model.Fingerprint;
