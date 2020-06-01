620be910863fc701209e7adcebbaf964fd3734e7
Sumit Sarin <sumitsarinofficial@gmail.com>
2020-06-01 20:20:34 +0530

    Remove redundant imports

--- plugin/src/test/java/io/jenkins/plugins/redis/RedisFingerprintStorageTest.java
+++ plugin/src/test/java/io/jenkins/plugins/redis/RedisFingerprintStorageTest.java
@@ -43,12 +43,10 @@ import redis.clients.jedis.Jedis;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
-import static org.hamcrest.core.StringContains.containsString;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.fail;
 
 public class RedisFingerprintStorageTest {
 
